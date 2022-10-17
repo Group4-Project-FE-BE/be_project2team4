12b511829c7f079e9e8a5e1a798a992dedb3bf67
lukmanul <klukmanul33@gmail.com>
2022-10-17 19:38:12 +0700

    add domain

--- feature/user/domain/domain.go
+++ feature/user/domain/domain.go
@@ -1,26 +1,28 @@
 package domain
 
 type Core struct {
-	ID     uint
-	Name   string
-	HP     string
-	Addres string
+	ID       uint
+	Name     string
+	Email    string
+	Phone    string
+	Addres   string
+	Bio      string
+	Gender   string
+	Location string
 }
 
 type Repository interface {
-	Insert(newUser Core) (Core, error)
+	Insert(newUser Core) (Core, error)  //register
+	GetUser(Email string) (Core, error) //login
 	Update(updatedData Core, ID uint) (Core, error)
 	Get(ID uint) (Core, error)
-	GetAll() ([]Core, error)
 	Delete(ID uint) (Core, error)
-	// GetUser(newUser Core) (Core, error)
 }
 
 type Service interface {
-	AddUser(newUser Core) (Core, error)
+	Register(newUser Core) (Core, error)
+	Login(Email string) (Core, error)
 	UpdateProfile(updatedData Core, ID uint) (Core, error)
 	Profile(ID uint) (Core, error)
-	ShowAllUser() ([]Core, error)
-	DeleteUser(ID uint) (Core, error)
-	// Login(newUser Core) (Core, error)
+	DeleteProfile(ID uint) (Core, error)
 }
