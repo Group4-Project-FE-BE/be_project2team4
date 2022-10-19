b0a387089a4f724f3be11cd586266041e2041105
lukmanul <klukmanul33@gmail.com>
2022-10-19 11:43:36 +0700

    editing yaml docker

--- config/config.go
+++ config/config.go
@@ -2,17 +2,18 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/labstack/gommon/log"
 )
 
 type AppConfig struct {
-	DBPort    uint
-	DBUser    string
-	DBPwd     string
-	DBHost    string
-	DBName    string
-	JWTSecret string
+	DBPort   uint
+	DBUser   string
+	DBPwd    string
+	DBHost   string
+	DBName   string
+	JWSecret string
 }
 
 func NewConfig() *AppConfig {
@@ -37,14 +38,14 @@ func initConfig() *AppConfig {
 	app.DBUser = os.Getenv("DB_USER")
 	app.DBPwd = os.Getenv("DB_PWD")
 	app.DBHost = os.Getenv("DB_HOST")
-	// port, err := strconv.Atoi(os.Getenv("DB_PORT"))
-	// if err != nil {
-	// 	log.Error("config error :", err.Error())
-	// 	return nil
-	// }
-	// app.DBPort = uint(port)
-	// app.DBName = os.Getenv("DB_NAME")
-	// app.JWTSecret = os.Getenv("JWT_SECRET")
+	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		log.Error("config error :", err.Error())
+		return nil
+	}
+	app.DBPort = uint(port)
+	app.DBName = os.Getenv("DB_NAME")
+	app.JWSecret = os.Getenv("JW_SECRET")
 
 	return &app
 }
